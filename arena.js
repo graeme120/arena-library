6c375669e0f19a7ae823d741d008d261158bf43f
graeme120 <graemem2414@gmail.com>
2023-11-19 21:23:07 -0500

    my version af

--- arena.js
+++ arena.js
@@ -1,85 +1,95 @@
 function parseMarkdown(text) {
-	// Regex function to parse out markdown in my channel
-	return text.replace(/\*\*(.*?)\*\*/g, '$1');
+  // Regex function to parse out markdown in my channel
+  return text.replace(/\*\*(.*?)\*\*/g, "$1");
 }
 
-// Wrap everything in an onload
-window.onload = async function() {
-	const CHANNEL_ID = 'YOUR-CHANNEL-NAME';
-	const ARENA_API_CHANNEL_URL = `https://api.are.na/v2/channels/${CHANNEL_ID}`;
-	const ARENA_API_CONTENT_URL = `https://api.are.na/v2/channels/${CHANNEL_ID}/contents?&per=100&sort=position&direction=desc&page=`;
-	
-	const contentDiv = document.getElementById('blockspace');
-	const template = document.getElementById('bookTemplate');
+window.onload = async function () {
+  const CHANNEL_ID = "library-_m__nu7eeiw";
+  const API_KEY = "lYrYVPXLwfC0k1WdWOXbqLrl8KlXw4ZEEBFkG5P1lH8";
+  const ARENA_API_CHANNEL_URL = `https://api.are.na/v2/channels/${CHANNEL_ID}`;
+  const ARENA_API_CONTENT_URL = `https://api.are.na/v2/channels/${CHANNEL_ID}/contents?&per=100&sort=position&direction=desc&page=`;
 
-	// Get channel data
+  const blockSpace = document.getElementById("blockspace");
+  const template = document.getElementById("bookTemplate");
+
   async function fetchChannelData() {
-		const response = await fetch(ARENA_API_CHANNEL_URL);
-		if (!response.ok) {
-			console.error(`Failed to fetch channel data: ${response.statusText}`);
-			return null;
-		}
-		return await response.json();
-	}
+    const response = await fetch(ARENA_API_CHANNEL_URL, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    });
+    if (!response.ok) {
+      console.error(`Failed to fetch channel data: ${response.statusText}`);
+      return null;
+    }
+    // console.log(response.json());
+    return await response.json();
+  }
 
-  
-	async function fetchBooksPage(page) {
-		const response = await fetch(`${ARENA_API_CONTENT_URL}${page}`);
-		if (!response.ok) {
-			console.error(`Failed to fetch page ${page}: ${response.statusText}`);
-			return null;
-		}
-		return await response.json();
-	}
+  async function fetchBooksPage(page) {
+    try {
+      const response = await fetch(`${ARENA_API_CONTENT_URL}${page}`, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      });
+      if (!response.ok) {
+        console.error(`Failed to fetch page ${page}: ${response.statusText}`);
+        return null;
+      }
+      return await response.json();
+    } catch (error) {
+      console.error(`Error fetching page ${page}: ${error}`);
+    }
+  }
 
-	const channelData = await fetchChannelData();
-	if (!channelData) {
-		console.error('Failed to fetch the channel data');
-		return;
-	}
+  const channelData = await fetchChannelData();
+  if (!channelData || !Array.isArray(channelData.contents)) {
+    console.error(
+      "Failed to fetch the channel data or data format is incorrect"
+    );
+    return;
+  }
 
-  // Calculate the number of pages required
-	const totalBlocks = channelData.length;
-	const perPage = 100;  // As per your original setting
-	const totalPages = Math.ceil(totalBlocks / perPage);
-	console.log(`Total pages: ${totalPages}`);
+  const totalPages = Math.ceil(channelData.contents.length / 100);
+  console.log(`Total pages: ${totalPages}`);
 
-  // Loop through all the pages
-	for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-		const pageData = await fetchBooksPage(currentPage);
-		if (!pageData) {
-			console.error(`Failed to fetch page ${currentPage}`);
-			continue;
-		}
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    const pageData = await fetchBooksPage(currentPage);
+    if (!pageData) {
+      console.error(`Failed to fetch page ${currentPage}`);
+      continue;
+    }
 
-    // Duplicate the template and append it to the page for each block
-		pageData.contents.forEach(item => {
-			if (item.class === 'Image') {
-					const clone = document.importNode(template.content, true);
-					clone.querySelector('.picture').src = item.image.original.url;
-					clone.querySelector('.book-title').textContent = item.title || '';
-					clone.querySelector('.book-description').textContent = item.description || '';
-					contentDiv.appendChild(clone);
-			}
+    pageData.contents.forEach((item) => {
+      if (item.class === "Attachment") {
+        const clone = document.importNode(template.content, true);
+        clone.querySelector(".book-link").href = item.attachment.url;
+        clone.querySelector(".picture").src = item.image.original.url;
+        clone.querySelector(".book-title").textContent = item.title || "";
+        clone.querySelector(".book-description").textContent =
+          item.description || "";
 
-			if (item.class === 'Text') {
-					const year = document.createElement('div');
-					year.classList.add('year-text');
-					year.classList.add('fade-in');
-					year.textContent = parseMarkdown(item.content);
-					contentDiv.appendChild(year);
-			}
-		});
-		
-		// Apply the animation effect
-		let delay = 1000;
-			Array.from(contentDiv.childNodes).forEach(child => {
-				if (child.nodeType === Node.ELEMENT_NODE) {
-					setTimeout(() => {
-							child.classList.add('visible');
-					}, delay);
-					delay += 50;
-				}
-			});
-	}
+        blockSpace.appendChild(clone);
+      }
+
+      if (item.class === "Text") {
+        const year = document.createElement("div");
+        year.classList.add("year-text");
+        year.textContent = parseMarkdown(item.content);
+        blockSpace.appendChild(year);
+      }
+    });
+
+    // Apply the animation effect
+    let delay = 500;
+    Array.from(blockSpace.childNodes).forEach((child) => {
+      if (child.nodeType === Node.ELEMENT_NODE) {
+        setTimeout(() => {
+          child.classList.add("visible");
+        }, delay);
+        delay += 50;
+      }
+    });
+  }
 };
